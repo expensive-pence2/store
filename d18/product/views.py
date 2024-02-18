874520365f40685339b855ecd4e7e9720f48c8df
Admin <hjk>
2024-02-18 17:17:28 +1000

    d19

--- d18/product/views.py
+++ d18/product/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context ={
+
+            'title': 'магааааааааз'
+        }
+
+    return render(request, 'index.html', context = context)
+
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'products' : [
+            {'image':'/static/vendor/img/products/Adidas-hoodie.png',
+            'name':'ivwue',
+            'price':'45692340562346582374776411326490',
+            'description':'сасен'},
+
+            {'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+            'name': 'uwfepov',
+            'price':'45692340562346582374776411326490',
+            'description': 'сасен'},
+
+            {'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+            'name': 'auhcfpso',
+            'price':'45692340562346582374776411326490',
+            'description': 'сасен'},
+
+            {'image': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png',
+            'name': 'qufoajic',
+            'price':'45692340562346582374776411326490',
+            'description': 'сасен'},
+
+            {'image': '/static/vendor/img/products/Black-Dr-Martens-shoes.png',
+            'name': 'atydiui',
+            'price':'45692340562346582374776411326490',
+            'description': 'сасен'},
+
+            {'image': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
+            'name': 'asdyoiujio',
+            'price':'45692340562346582374776411326490',
+            'description': 'сасен'},
+    ]
+    }
+    return render(request, 'products.html', context = context)
